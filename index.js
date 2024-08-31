f06bd888fe75552153b17fc1b01ae6dd642e887c
Karl Aparece <aparecekarl@yahoo.com>
2024-08-31 10:46:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Subdomain takeover of Zircuit Assets");
+  res.send("Subdomain takeover for Bug Bounty Report");
 });
 
 app.listen(process.env.PORT || 5000);
